7e6d003f528b8188591756aad2b5d06add73166f
shivasfotedar <57842435+shivasfotedar@users.noreply.github.com>
2023-03-12 18:05:03 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,15 +1,14 @@
 variable "region" {
 }
 
-variable "demo_instance_name"{
+variable "demo_blue_instance_name"{
 }
-variable "demo_security_name" {
+variable "demo_blue_security_name" {
 }
 variable "demo_key_name" {
 }
-variable "shell_file" {
+variable "shell_file_blue" {
 }
-
 variable "XL" {
 }
 variable "L" {
@@ -18,3 +17,14 @@ variable "M" {
 }
 variable "S" {
 }
+variable "ami_id" {}
+variable "artifact_name" {}
+
+
+
+variable "vpc_name" {}
+variable "cidr_block" {}
+variable "private_subnet_cidr_blocks" {}
+variable "public_subnet_cidr_blocks" {}
+variable "environment" {}
+variable "elb_name" {}
